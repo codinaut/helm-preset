08f996cea8b5ac33f57037ad0be9f1bb5f0e2107
Daniel <codinaut@gmail.com>
2020-08-12 02:48:36 +0700

    feat: empty presets into configuration

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,1 +1,38 @@
 pub mod manifest;
+
+use snafu::Snafu;
+
+#[derive(Debug, Snafu)]
+pub enum Error {
+}
+
+pub struct Entry {
+}
+
+pub type Configuration = Vec<Vec<Entry>>;
+
+impl manifest::Manifest {
+    pub fn into_config(self) -> Result<Configuration, Error> {
+        Ok(vec![])
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn empty_presets() {
+        let config = serde_yaml::from_str::<manifest::Manifest>(&format!(
+            r#"
+            categories:
+              - a
+              - b
+            "#,
+        ))
+        .unwrap()
+        .into_config()
+        .unwrap();
+        assert_eq!(config.len(), 0)
+    }
+}
