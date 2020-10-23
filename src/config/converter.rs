33fc5e164f2fa393873bae0eb3ea12fdb1ca9ea0
Daniel <codinaut@gmail.com>
2020-10-24 02:39:31 +0700

    feat: parse inline manifests

--- src/config/converter.rs
+++ src/config/converter.rs
@@ -1,27 +1,72 @@
+use super::{Category, Manifest};
+use serde_yaml::Mapping;
 use snafu::Snafu;
-
-use super::Manifest;
 
 #[derive(Debug, Snafu)]
 pub enum Error {}
 
-pub struct Entry {}
+#[derive(Debug, Eq, PartialEq)]
+pub struct Entry {
+    name: String,
+    values: Mapping,
+}
 
 pub type Configuration = Vec<Vec<Entry>>;
 
 impl Manifest {
     pub fn into_config(self) -> Result<Configuration, Error> {
-        Ok(vec![])
+        Ok(self
+            .categories
+            .into_iter()
+            .filter_map(|category| match category {
+                Category::NameOnly(_) => None,
+                Category::Explicit(explicit_category) => Some(
+                    explicit_category
+                        .presets
+                        .into_iter()
+                        .map(|(name, values)| Entry { name, values })
+                        .collect(),
+                ),
+            })
+            .collect())
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use maplit::hashmap;
+    use serde_yaml::{Mapping, Value::String};
+    use std::iter::FromIterator;
 
     #[test]
     fn empty() {
         let config = Manifest::default().into_config().unwrap();
         assert_eq!(config.len(), 0)
+    }
+
+    #[test]
+    fn inline() {
+        let config = serde_yaml::from_str::<Manifest>(
+            r"
+                categories:
+                  - name: category-1
+                    presets:
+                      key-1:
+                        a: b
+            ",
+        )
+        .unwrap()
+        .into_config()
+        .unwrap();
+        assert_eq!(
+            config,
+            vec![vec![Entry {
+                name: "key-1".to_string(),
+                values: Mapping::from_iter(hashmap! {
+                    String("a".to_string()) => String("b".to_string()),
+                })
+            }]]
+        )
     }
 }
