4a61074fde90f692cf3f1b57c0eb195a6b4286ca
Daniel <codinaut@gmail.com>
2020-10-24 16:48:43 +0700

    refactor: naming on deep_merge

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,8 @@
 use serde_yaml::Value;
 
-fn deep_merge(target: &mut &mut Value, override_value: Value) {
+fn deep_merge(target: &mut &mut Value, substitute: Value) {
     if let Value::Mapping(map) = *target {
-        if let Value::Mapping(override_map) = override_value {
+        if let Value::Mapping(override_map) = substitute {
             for (k, v) in override_map {
                 map.insert(k, v);
             }
@@ -19,38 +19,42 @@ mod test {
     fn deep_merge_replaces_non_map_with_non_map_substitute() {
         let mut target = Value::String("value-1".to_string());
         let substitute = Value::String("value-replacement-1".to_string());
+
         deep_merge(&mut &mut target, substitute.clone());
         assert_eq!(target, substitute)
     }
 
     #[test]
-    fn merge_with_empty() {
-        let target = serde_yaml::from_str::<Value>(
+    fn deep_merge_map_with_empty_map_is_no_op() {
+        let mut target = serde_yaml::from_str::<Value>(
             r"
                 a: b
             ",
         )
         .unwrap();
-        let mut source = target.clone();
-        deep_merge(&mut &mut source, Value::Mapping(Mapping::new()));
-        assert_eq!(source, target)
+        let substitute = Value::Mapping(Mapping::new());
+        let conclusion = target.clone();
+
+        deep_merge(&mut &mut target, substitute);
+        assert_eq!(target, conclusion)
     }
 
     #[test]
-    fn merge_with_replacement() {
-        let mut source = serde_yaml::from_str::<Value>(
+    fn deep_merge_map_with_map_returns_complement() {
+        let mut target = serde_yaml::from_str::<Value>(
             r"
                 a: b
             ",
         )
         .unwrap();
-        let target = serde_yaml::from_str::<Value>(
+        let substitute = serde_yaml::from_str::<Value>(
             r"
                 a: c
             ",
         )
         .unwrap();
-        deep_merge(&mut &mut source, target.clone());
-        assert_eq!(source, target)
+
+        deep_merge(&mut &mut target, substitute.clone());
+        assert_eq!(target, substitute)
     }
 }
